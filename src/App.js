5405033f2bf2fde0910ee3d5e2d9719051605078
snoringarvind <snoringarvind@gmail.com>
2020-12-05 03:23:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
+import Nav from "./components/Nav";
+import Pay from "./components/Pay";
+import { CartProvider } from "./components/CartContext";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/shop" component={Shop} />
+            <Route path="/cart" exact component={Cart} />
+            <Route path="/cart/checkout" exact component={Pay} />
+          </Switch>
+        </div>
+      </Router>
+    </CartProvider>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+// var names = ["Daniel", "Lucas", "Gwen", "Henry", "Jasper", "Lucas", "Daniel"];
+
+// const uniqueNames = names.filter((val, id, array) => {
+//   console.log(val, id);
+//   console.log(array.indexOf(val), id);
+//   return array.indexOf(val) === id;
+// });
+// console.log(uniqueNames);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
