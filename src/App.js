8bba3830ceb91c36dc827311101a33b42fccbf6f
snoringarvind <snoringarvind@gmail.com>
2020-12-06 16:31:24 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Shop from "./components/Shop/Shop";
 import Cart from "./components/Cart/Cart";
